473aa8af6c08f4945ef68a6bddf69665ed64cb62
Albertchen857 <albertchen857@outlook.com>
2024-10-20 21:28:13 +1000

    LitematicaViewer Beta 0.1.4 | add icon and add bg color

--- LitematicaViewer.py
+++ LitematicaViewer.py
@@ -22,7 +22,7 @@ def import_file():
     file_path = file_path.replace("\\", "/")
     print(f"Imported file: {file_path}")
     file_name = file_path.split("/")[-1]
-    label_bottom.config(text=f"{file_name}")
+    label_middle.config(text=f"{file_name}")
 
 def start_analysis(simple_type=False):
     count_table.delete(*count_table.get_children())
@@ -40,24 +40,26 @@ def start_analysis(simple_type=False):
             size_x = region.maxx() - region.minx()
             size_y = region.maxy() - region.miny()
             size_z = region.maxz() - region.minz()
-            label_bottom2.config(text=f"{size_x}x{size_y}x{size_z}")
-            # Analyze blocks
+            label_bottom.config(text=f"{size_x}x{size_y}x{size_z}")
             for x in range(size_x + 1):
                 for y in range(size_y + 1):
                     for z in range(size_z + 1):
-                        block_state = region._Region__palette[region._Region__blocks[x, y, z]]
-                        block_id = block_state._BlockState__block_id
-                        output = block_state
-                        if block_id != "minecraft:air":
-                            if block_id == "minecraft:piston_head" or block_id == "minecraft:bubble_column":
-                                continue
-                            if simple_type:
-                                output = block_id
-                            print(block_id)
-                            if output not in Block:
-                                Block[output] = 1
-                            else:
-                                Block[output] += 1
+                        try:
+                            block_state = region._Region__palette[region._Region__blocks[x, y, z]]
+                            block_id = block_state._BlockState__block_id
+                            output = block_state
+                            if block_id != "minecraft:air":
+                                if block_id == "minecraft:piston_head" or block_id == "minecraft:bubble_column":
+                                    continue
+                                if simple_type:
+                                    output = block_id
+                                if output not in Block:
+                                    Block[output] = 1
+                                else:
+                                    Block[output] += 1
+                        except Exception as e:
+                            print(f"Error Entites during analysis: {e}")
+                            continue
     except Exception as e:
         print(f"Error during analysis: {e}")
         return
@@ -89,50 +91,58 @@ def show_block_count(sorted_block, simple_type):
 
 # Tkinter Setting
 litem = tk.Tk()
-litem.title("Litematica Viewer")
+litem.title("Litematica Viewer投影查看器")
 litem.geometry("720x720")
 litem.iconbitmap("icon.ico")
+litem.configure(bg="#3E3E3E")
 
 # 上容器
 frame_top = tk.Frame(litem)
+frame_top.configure(bg="#3E3E3E")
 frame_top.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
 
-btn_import = tk.Button(frame_top, text="Import", command=import_file)
+btn_import = tk.Button(frame_top, text="Import导入", command=import_file)
 btn_import.pack(side=tk.LEFT, padx=5, pady=5)
 
-btn_start = tk.Button(frame_top, text="Start Analysis(id & properties)", command=start_analysis)
+btn_start = tk.Button(frame_top, text="Start Analysis(id & properties)分析", command=start_analysis)
 btn_start.pack(side=tk.LEFT, padx=5, pady=5)
 
-btn_simstart = tk.Button(frame_top, text="Simple Analysis(block name)", command=lambda:start_analysis(True))
+btn_simstart = tk.Button(frame_top, text="Simple Analysis(block name)简洁分析", command=lambda:start_analysis(True))
 btn_simstart.pack(side=tk.LEFT, padx=5, pady=5)
 
 icon = tk.PhotoImage(file="block/crafting_table.png")
 icon_image = tk.Label(frame_top, image=icon)
 icon_image.pack(side=tk.RIGHT, padx=5, pady=5)
 
+table_sty = ttk.Style()
+table_sty.configure("Treeview", font=("Helvetica", 10), rowheight=25, background="#1F1F1F", foreground="white")
+table_sty.configure("Treeview.Heading", font=("Minecraft", 12, "bold"))
+table_sty.map('Treeview', background=[('selected', 'blue')])
 # 中容器
 frame_middle = tk.Frame(litem, bg="white", height=400)
 frame_middle.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=5, pady=5)
-label_middle = tk.Label(frame_middle, text="Block Count List", font=("Helvetica", 24, "bold"))
-label_middle.pack()
+frame_middle.configure(background="#2F2F2F")
+label_middle = tk.Label(frame_middle, text="114514.litematica", font=("Helvetica", 18))
+label_middle.configure(bg="#3E3E3E", fg="white")
+label_middle.pack(padx=5, pady=20)
 
-count_table = ttk.Treeview(frame_middle, column=('blockID',  '数量', '数量（组）', '属性'), height=7)
+count_table = ttk.Treeview(frame_middle, column=('blockID',  'num', 'unit', 'properties'), height=7)
 count_table.heading('blockID', text='blockID', anchor="center")
-count_table.heading('数量', text='数量', anchor="center")
-count_table.heading('数量（组）', text='数量（组）', anchor="center")
-count_table.heading('属性', text='属性', anchor="center")
+count_table.heading('num', text='数量', anchor="center")
+count_table.heading('unit', text='数量/单位', anchor="center")
+count_table.heading('properties', text='属性', anchor="center")
 count_table.column("#0", width=10, anchor="e")
 count_table.column("blockID", width=50)
-count_table.column("数量" , width=2)
-count_table.column("数量（组）", width=20)
-count_table.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=50, pady=20)
+count_table.column("num" , width=2)
+count_table.column("unit", width=30)
+count_table.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=50, pady=10)
 
 # 下容器
 frame_bottom = tk.Frame(litem)
 frame_bottom.pack(side=tk.BOTTOM, fill=tk.X, padx=5, pady=5)
-label_bottom = tk.Label(frame_bottom, text="114514.litematica", font=("Helvetica", 12, "italic"))
+frame_bottom.configure(bg="#3E3E3E")
+label_bottom = tk.Label(frame_bottom, text="Size", font=("Helvetica", 14))
+label_bottom.configure(bg="#3E3E3E", fg="white")
 label_bottom.pack()
-label_bottom2 = tk.Label(frame_bottom, text="Size", font=("Helvetica", 10))
-label_bottom2.pack()
 
 litem.mainloop()
---
+++ sw
@@ -0,0 +1,29 @@
+[33mcommit bd351b8cb1519bc9aa73717a7f9dcda01cfd4181[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: Albertchen857 <albertchen857@outlook.com>
+Date:   Sat Oct 19 22:25:48 2024 +1000
+
+    LitematicaViewer Beta0.1.3 | add More Icons and fixed bug
+
+[33mcommit 81a2ba548bddb0f457440f6aaf46ca66f415006e[m
+Author: Albertchen857 <albertchen857@outlook.com>
+Date:   Sat Oct 19 10:13:08 2024 +1000
+
+    LitematicaViewer Beta0.1.2 | add Icon and simplfied mode
+
+[33mcommit 7165896511bc55276a45a8a0a5f28de3c5bf936e[m
+Author: Albertchen857 <albertchen857@outlook.com>
+Date:   Sat Oct 19 10:11:01 2024 +1000
+
+    git add .
+
+[33mcommit 3ac32b176f634986ac8fe80fc0a9c631d0078a9f[m
+Author: Albertchen857 <albertchen857@outlook.com>
+Date:   Wed Oct 16 16:48:04 2024 +1000
+
+    litematica viewer beta 0.1.1
+
+[33mcommit 847c600dd44e7d9faa4237c86914950db0b9f0ad[m
+Author: Albertchen857 <albertchen857@outlook.com>
+Date:   Tue Oct 15 22:40:58 2024 +1000
+
+    litematicaviewer beta 1.0
